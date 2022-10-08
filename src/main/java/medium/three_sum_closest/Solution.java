56c0ac735b15b47d97a6b97e23af7b7bebdba144
Tenagrim <tenagrim@gmail.com>
2022-10-08 19:52:21 +0500

    three sum closest sort + two pointer alg

--- src/main/java/medium/three_sum_closest/Solution.java
+++ src/main/java/medium/three_sum_closest/Solution.java
@@ -1,24 +1,22 @@
 package medium.three_sum_closest;
 
+import java.util.Arrays;
+
 public class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        int len = nums.length;
-        int closest = nums[0] + nums[1] + nums[2], suspect, dist;
-        if (len == 3) return closest;
-        for (int i = 0; i < len; i++) {
-            for (int j = 0; j < len; j++) {
-                for (int k = 0; k < len; k++) {
-                    if (i!=j&&i!=k&&j!=k){
-                        suspect = nums[i] + nums[j] + nums[k];
-                        dist = Math.abs(target - suspect);
-                        if (dist == 0)
-                            return suspect;
-                        else if (Math.abs(target -closest) > dist) {
-                            closest = suspect;
-                        }
-                    }
-                }
-            }
+        int  len = nums.length, closest = nums[0] + nums[1] + nums[len-1];
+        Arrays.sort(nums); // O(NlogN)
+        for (int i = 0; i < len -2; i++) {
+            int first = nums[i], second_ind = i + 1, third_ind = len-1, sum;
+            do{
+                sum = first + nums[second_ind] + nums[third_ind];
+                if (Math.abs(target - sum) < Math.abs(target - closest) || sum == target)
+                    closest = sum;
+                if (sum < closest)
+                    second_ind ++;
+                else
+                    third_ind--;
+            }while (second_ind < third_ind  && sum != target);
         }
         return closest;
     }
