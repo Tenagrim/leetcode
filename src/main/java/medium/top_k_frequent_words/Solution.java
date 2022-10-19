b335e613fcf71d76f328eecbd65bef2419c60e37
Tenagrim <tenagrim@gmail.com>
2022-10-19 10:36:38 +0300

    Top K Frequent Words

--- src/main/java/medium/top_k_frequent_words/Solution.java
+++ src/main/java/medium/top_k_frequent_words/Solution.java
@@ -6,15 +6,14 @@ import java.util.stream.Collectors;
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
         Map<String, Integer> rates = new HashMap<>();
-        for (String w : words) {
-            Integer rate = rates.get(w);
-            if (rate == null)
-                rates.put(w, 1);
-            else rates.put(w, rate + 1);
-        }
+        for (String w : words)
+            rates.merge(w, 1, Integer::sum);
 
         return rates.entrySet().stream()
-                .sorted((el1,el2)->{return el2.getValue() - el1.getValue()==0 ? el1.getKey().compareTo(el2.getKey()) : el2.getValue() - el1.getValue();})
+                .sorted((el1, el2) -> {
+                    int diff = el2.getValue() - el1.getValue();
+                    return diff == 0 ? el1.getKey().compareTo(el2.getKey()) : diff;
+                })
                 .limit(k)
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toList());
